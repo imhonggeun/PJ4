e409fbd1b3a30f81c197f4a277acfe13539237cb
imhonggeun <dkfl9870@gmail.com>
2025-07-16 23:33:08 +0900

    프로젝트 및 파일 구조 생성

---
+++ src/main/java/com/java/DTO/HomeDTO.java
@@ -0,0 +1,5 @@
+package com.java.DTO;
+
+public class HomeDTO {
+
+}
---
+++ src/main/java/com/java/Pj4Application.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pj4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pj4Application.class, args);
+	}
+
+}
